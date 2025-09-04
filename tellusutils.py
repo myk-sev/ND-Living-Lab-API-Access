bdc053ea25dfdc783fb38beea3de63664d1738f0
myk-sev <sev.myk@outlook.com>
2025-09-04 13:04:27 -0500

    leftover file removal

--- tellusutils.py
+++
@@ -1,6 +0,0 @@
-import datetime
-
-def time_formatter_tellus(dt_str):
-    guide = '%Y-%m-%dT%H:%M:%S%+%H:%M'
-    dt_obj = datetime.datetime.strptime(dt_str, guide)
-    tellus_time = datetime.datetime.strftime()
